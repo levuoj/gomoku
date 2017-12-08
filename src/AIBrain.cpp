cbd50ebdee4b1b3c2be46e78844696f725b9976c
Anthony Jouvel <anthony.jouvel@epitech.eu>
2017-12-08 16:24:55 +0100

    remove unused func

--- src/AIBrain.cpp
+++ src/AIBrain.cpp
@@ -23,40 +23,6 @@ void    AIBrain::setType(ESquareType type)
     _type = type;
 }
 
-int     AIBrain::minimax(int depth, int nodeIndex, bool maximizePlayer, std::vector<int> values, int alpha, int beta)
-{
-    if (depth == DEPTH)
-        return (values[nodeIndex]);
-    if (maximizePlayer)
-    {
-        int best = MIN;
-		for (int i = 0; i < 2; i++)
-		{
-			int val = minimax(depth + 1, nodeIndex * 2 + 1,
-				false, values, alpha, beta);
-			best = std::max(best, val);
-			alpha = std::max(alpha, best);
-			if (beta <= alpha)
-				break;
-		}
-		return (best);
-    }
-    else
-    {
-        int best = MAX;
-		for (int i = 0; i < 2; i++)
-		{
-			int val = minimax(depth + 1, nodeIndex * 2 + 1,
-				true, values, alpha, beta);
-			best = std::min(best, val);
-			beta = std::min(beta, best);
-			if (beta <= alpha)
-				break;
-		}
-        return (best);
-    }
-}
-
 std::string const     AIBrain::play()
 {
     _moveManager.findMoves();
